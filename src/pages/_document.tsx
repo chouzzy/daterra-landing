9d89a7768ac8307a223ff3ff9d038911042699c7
Matheus <mathfernando@hotmail.com>
2022-07-04 14:00:42 -0300

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
          <Head>
          <link rel="preconnect" href="https://fonts.googleapis.com"/>
          <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700&family=Nanum+Gothic:wght@400;700;800&display=swap" rel="stylesheet"/>
-         <link rel="shortcut icon" href="/static/img/dat-icon.png" /> 
+         <link rel="shortcut icon" href="/static/img/dat-icon.ico" /> 
          </Head>
             <body>
                <Main/>
