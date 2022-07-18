d488c3b3d463b46492f60566c1ee27a2b88c1bb8
Matheus <mathfernando@hotmail.com>
2022-07-18 15:32:53 -0300

    link fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,25 @@ export default class MyDocument extends Document {
       return (
          <Html>
          <Head>
+          {/* <!-- HTML Meta Tags --> */}
+          <meta name="description" content="O toque de beleza que faltava no seu dia"/>
+
+         {/* <!-- Facebook Meta Tags --> */}
+         <meta property="og:url" content="https://revenda.daterra.com.br/"/>
+         <meta property="og:type" content="website"/>
+         <meta property="og:title" content="Daterra - Qualidade que se sente"/>
+         <meta property="og:description" content="Qualidade que se sente"/>
+         <meta property="og:image" content="https://revenda.daterra.com.br/static/img/daterra-logo.png"/>
+
+         {/* <!-- Twitter Meta Tags --> */}
+         <meta name="twitter:card" content="summary_large_image"/>
+         <meta property="twitter:domain" content="daterra.com.br"/>
+         <meta property="twitter:url" content="https://revenda.daterra.com.br//"/>
+         <meta name="twitter:title" content="Daterra - Qualidade que se sente"/>
+         <meta name="twitter:description" content="O toque de beleza que faltava no seu dia"/>
+         <meta name="twitter:image" content="https://revenda.daterra.com.br/static/img/daterra-logo.png"/>
+
+
          <link rel="preconnect" href="https://fonts.googleapis.com"/>
          <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700&family=Nanum+Gothic:wght@400;700;800&display=swap" rel="stylesheet"/>
          <link rel="shortcut icon" href="/static/img/dat-icon.ico" /> 
